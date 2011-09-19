caa568e118420ee877e603b55862ccf63e0ba13c
Peter Vanbroekhoven <peter@xaop.com>
2011-09-19 13:01:37 +0200

    Added correct jar Added method to extract the content

--- lib/tika4r.rb
+++ lib/tika4r.rb
@@ -1,5 +1,7 @@
 module Tika4R
   
-  
+  def self.extract_content(filename)
+    `java -jar #{File.join(File.dirname(__FILE__), "..", "vendor", "tika-app-0.9.jar").inspect} -t #{filename.inspect}`
+  end
   
 end
