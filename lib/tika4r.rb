d9cac658cef938fa5e3f0e5acfef983fb8d58e6b
Joeri Samson <joeri@xaop.com>
2011-11-09 16:32:09 +0100

    Try to make exceptions reraisable
    This isn't perfect yet, we probably should have a Tika4r::Error class,
    but at least this will make error reports 'usable'

--- lib/tika4r.rb
+++ lib/tika4r.rb
@@ -5,11 +5,23 @@ module Tika4R
     file = Rjb.import("java.io.File").new(filename)
     tika = Rjb.import("org.apache.tika.Tika")
 
-    # Even if we could avoid loading everything here by parsing to a Reader this 
-    # still wouldn't help us as the Rsolr plugin builds XML by using Builder, and
-    # it needs to be a full string by then (i.e. the XML this will be in is built
-    # in memory anyway)
-    tika.new._invoke("parseToString", "Ljava.io.File;", file)
+    begin
+      # Even if we could avoid loading everything here by parsing to a Reader this 
+      # still wouldn't help us as the Rsolr plugin builds XML by using Builder, and
+      # it needs to be a full string by then (i.e. the XML this will be in is built
+      # in memory anyway)
+      tika.new._invoke("parseToString", "Ljava.io.File;", file)
+    rescue StandardError => e
+      # Trying to make TikaExceptions reraisable (also does it for all Java errors)
+      begin
+        e.to_str
+      rescue StandardError
+        def e.to_str
+          to_s
+        end
+      end
+      raise e
+    end
   end
   
 
