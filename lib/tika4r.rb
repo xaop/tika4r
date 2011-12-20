275fb5222b5d318b59bd162e244600b0f59f9b5b
Joeri Samson <joeri@xaop.com>
2011-12-20 10:41:00 +0100

    Try to prevent undefined constant error
    Suspicion: Catching errors only worked for TikaExceptions, otherwise
    that constant wasn't defined.

--- lib/tika4r.rb
+++ lib/tika4r.rb
@@ -6,6 +6,7 @@ module Tika4R
     load_rjb
     file = Rjb.import("java.io.File").new(filename)
     tika = Rjb.import("org.apache.tika.Tika")
+    tikaException = Rjb.import("org.apache.tika.exception.TikaException")
 
     begin
       # Even if we could avoid loading everything here by parsing to a Reader this 
@@ -13,7 +14,7 @@ module Tika4R
       # it needs to be a full string by then (i.e. the XML this will be in is built
       # in memory anyway)
       tika.new._invoke("parseToString", "Ljava.io.File;", file)
-    rescue TikaException => e
+    rescue tikaException => e
       raise Tika4R::Exception.new(e)
     ensure
       GC.start
